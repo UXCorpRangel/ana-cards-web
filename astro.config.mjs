a9fad71e49ddd4d08861528c5f301a0e948ea707
Jaime Alan Gutiérrez Cruz <33694882+jagcruz@users.noreply.github.com>
2024-07-13 15:42:21 -0300

    chore: :construction: add site URL to `astro.config.mjs`

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://UXCorpRangel.github.io/ana-cards-web',
   vite: {
     build: {
       cssMinify: 'lightningcss'
