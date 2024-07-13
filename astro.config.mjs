61a6babc5d51c31006a44f75db78c4965dbe90e3
Jaime Alan Gutiérrez Cruz <33694882+jagcruz@users.noreply.github.com>
2024-07-13 15:55:58 -0300

    fix: :green_heart: update base URL in `astro.config.mjs`

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://UXCorpRangel.github.io/ana-cards-web',
+  base: '/ana-cards-web',
   vite: {
     build: {
       cssMinify: 'lightningcss'
