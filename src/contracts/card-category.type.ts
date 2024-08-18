344d3061d1ad941ecc5ce25aa89245db030584d1
odracirdev <ricardocuauro93@gmail.com>
2024-08-18 04:22:34 -0400

    feat: :sparkles: add special category

--- src/contracts/card-category.type.ts
+++ src/contracts/card-category.type.ts
@@ -11,7 +11,9 @@ export enum CardCategory {
   /** Categoría de troll */
   TROLL = 'troll',
   /** Categoría de herramientas */
-  TOOLS = 'tools'
+  TOOLS = 'tools',
+  /** Categoría especial */
+  SPECIAL = 'special'
 }
 
 /** Mapa de categorías de las cartas */
@@ -19,12 +21,13 @@ type CategoryMap = Record<CardCategory, string>;
 
 /** Etiquetas de las categorías de las cartas */
 export const cardCategoryLabels: CategoryMap = {
-  [CardCategory.DESIGN]: 'Diseño',
-  [CardCategory.DEVELOPMENT]: 'Desarrollo',
+  [CardCategory.DESIGN]: 'Design',
+  [CardCategory.DEVELOPMENT]: 'Development',
   [CardCategory.HARLEY]: 'Harley',
   [CardCategory.LORE]: 'Lore',
   [CardCategory.TROLL]: 'Troll',
-  [CardCategory.TOOLS]: 'Herramientas'
+  [CardCategory.TOOLS]: 'Tools',
+  [CardCategory.SPECIAL]: 'Special'
 };
 
 /** Colores de las categorías de las cartas */
@@ -34,5 +37,7 @@ export const cardCategoryColors: CategoryMap = {
   [CardCategory.HARLEY]: 'var(--harley-color)',
   [CardCategory.LORE]: 'var(--lore-color)',
   [CardCategory.TROLL]: 'var(--troll-color)',
-  [CardCategory.TOOLS]: 'var(--tools-color)'
+  [CardCategory.TOOLS]: 'var(--tools-color)',
+  [CardCategory.SPECIAL]:
+    'linear-gradient(153deg, var(--design-color) 0%, var(--harley-color) 20%, var(--development-color) 41.86%, var(--lore-color) 59.84%, var(--troll-color) 75.37%, var(--tools-color) 100%)'
 };
