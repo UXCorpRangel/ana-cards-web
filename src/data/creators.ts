aef1219e1bcc2d4efb3dafc785ecfb8f35ace7ef
odracirdev <ricardocuauro93@gmail.com>
2024-10-29 20:04:43 -0300

    feat: :busts_in_silhouette: add snowy information

--- src/data/creators.ts
+++ src/data/creators.ts
@@ -5,7 +5,7 @@ export const creators: Creator[] = [
     avatar: 'anarangel.avif',
     name: 'Ana Rangel',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde corrupti incidunt nihil dolore qui magni deserunt placeat beatae minus, quas, accusamus nobis tenetur saepe, error dignissimos excepturi cupiditate vitae quis!',
+      'UX Designer con una comunidad tan avanzada que ya es casi una secta, de hecho, mis seguidores llevan mi apellido en sus nicknames. Amante de los unicornios, devs y todo lo que tenga que ver con jugar en equipo (o sea, Fortnite y juegos a fines). Me encontrarás siempre rompiendo el estereotipo del UX gruñón: en lugar de hacerle la vida imposible a los devs, prefiero enseñar, compartir risas, y conquistar corazones... Si buscas unicornios, buena onda, y algo de código, estás oficialmente en territorio UX Corp Rangel!',
     links: [
       { href: 'https://anarangel.github.io', icon: 'website' },
       { href: 'https://github.com/anarangel', icon: 'github' },
@@ -23,8 +23,9 @@ export const creators: Creator[] = [
     avatar: 'odracirdev.avif',
     name: 'Ricardo Cuauro',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde corrupti incidunt nihil dolore qui magni deserunt placeat beatae minus, quas, accusamus nobis tenetur saepe, error dignissimos excepturi cupiditate vitae quis!',
+      'Desarrollador frontend en constante aprendizaje autodidacta, moderador del canal de Ana, trabajador explotado (por él mismo) durante el día y streamer de noche. Amante del café y el código vanilla pero siempre dispuesto a probar nuevas tecnologías. Colaborador en distintas comunidades siempre dispuesto a aportar líneas de código, buena onda y mucha joda. Recientemente nombrado como TL por Ana aunque siempre le pide ayuda a su buen amigo Gepeto.',
     links: [
+      { href: 'https://odracir.dev', icon: 'website' },
       { href: 'https://github.com/odracirdev', icon: 'github' },
       { href: 'https://linkedin.com/in/RicardoCuauro', icon: 'linkedin' },
       { href: 'https://twitch.tv/odracirdev', icon: 'twitch' },
@@ -37,7 +38,7 @@ export const creators: Creator[] = [
     avatar: 'jagcruz.avif',
     name: 'Jaime Gutiérrez',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde corrupti incidunt nihil dolore qui magni deserunt placeat beatae minus, quas, accusamus nobis tenetur saepe, error dignissimos excepturi cupiditate vitae quis!',
+      '¡Quita coño! Nuestro senior gruñón (es de cariño, no lo hace adrede). Si no aprendes por las buenas seguramente aprenderás por las malas. El conocimiento es su poder y lo reparte a diestra y siniestra con toda la comunidad. Es la persona encargada de administrar el papel higiénico en el sótano donde todos trabajamos, por esto su rol es indispensable para el correcto funcionamiento de la corporación. El encargado de consultarle a Sandy si aprueba las PR´s o no.',
     links: [
       { href: 'https://jagcruz.github.io', icon: 'website' },
       { href: 'https://github.com/jagcruz', icon: 'github' },
@@ -48,7 +49,7 @@ export const creators: Creator[] = [
     birthdate: '07-09'
   },
   {
-    avatar: 'felixicaza.avif?v=1',
+    avatar: 'felixicaza.avif',
     name: 'Félix Icaza',
     description:
       'Uno de los primeros integrantes del sótano. Nadie aprueba una PR sin consultarle, es capaz de sacarle el máximo provecho a la web, conocido por tener cierta destreza con CSS y ganador de las cartas de Ana Rangel y Figma en ManzDev. Ahora su próxima meta es lograr la carta de Figma en este proyecto antes que Ana.',
@@ -63,12 +64,24 @@ export const creators: Creator[] = [
     avatar: 'omaaraguirre.avif',
     name: 'Omar Aguirre',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde corrupti incidunt nihil dolore qui magni deserunt placeat beatae minus, quas, accusamus nobis tenetur saepe, error dignissimos excepturi cupiditate vitae quis!',
+      'El alma de la fiesta, nuestro mexican sexy boy, la voz que nos pone a temblar en Discord y la mente detrás de la mayoría de los easter eggs de todos nuestros trabajos. Todo esto comprimido en 1.6mts de estatura gracias a que es uno de los pocos que paga la licencia de WinRAR.',
     links: [
       { href: 'https://omao.dev', icon: 'website' },
       { href: 'https://github.com/omaaraguirre', icon: 'github' },
       { href: 'https://linkedin.com/in/omaaraguirre', icon: 'linkedin' }
     ],
     birthdate: '03-09'
+  },
+  {
+    avatar: 'luciano-morales.avif',
+    name: 'Luciano Morales',
+    description:
+      'El primer moderador del canal y también fundador del mismo, el usuario más tierno del chat y el que más le sabe a la parte trasera de la web. En el sótano es conocido como "el cochino" por las cochinadas que es capaz de hacer en el VSCode.',
+    links: [
+      { href: 'https://www.luciano-morales.com/', icon: 'website' },
+      { href: 'https://github.com/luc-mo', icon: 'github' },
+      { href: 'https://www.linkedin.com/in/lucmoralesr', icon: 'linkedin' }
+    ],
+    birthdate: '04-27'
   }
 ];
